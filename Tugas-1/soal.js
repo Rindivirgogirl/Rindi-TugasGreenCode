2140ba9fae2ae68fd8b33fdcb2f1b5bb2e009630
Rindivirgogirl <rindicutee325@gmail.com>
2023-10-04 16:31:14 +0700

    add Tugas-1

---
+++ Tugas-1/soal.js
@@ -0,0 +1,16 @@
+let Nama = "Rindiani"
+let Usia = 20
+let Tinggi= 150
+
+let bilangan1 = 8
+let bilangan2 = 9
+let bilangan3 = 10
+
+let isStudent= true
+let isStudent1= false
+
+console.log("hasilpenjumlahan=", bilangan1 + bilangan2)
+console.log("hasilpengurangan=", bilangan2 - bilangan3)
+console.log("usia dan tinggi=", Usia * Tinggi)
+console.log(isStudent)
+console.log(isStudent1)
