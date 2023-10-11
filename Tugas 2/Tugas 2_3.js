d9291fb35a8a52c62f0c87c65c23a365b4a5dd9e
Rindivirgogirl <rindicutee325@gmail.com>
2023-10-11 16:16:29 +0700

    add Tugas 2_3

---
+++ Tugas 2/Tugas 2_3.js
@@ -0,0 +1,13 @@
+let nilaiku = 75
+if (nilaiku>90) {
+console.log("sangat bagus")
+
+}else if (nilaiku <90 && nilaiku>80){
+console.log("bagus")
+
+}else if (nilaiku <80 && nilaiku >70){
+console.log("cukup bagus")
+
+}else if (nilaiku <70){
+console.log("kurang bagus")
+}
