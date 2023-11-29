361b25df79ac3e5051bf440d1ec9cc3cfd723ac9
Rindivirgogirl <rindicutee325@gmail.com>
2023-11-29 12:58:20 +0700

    add-Tugas9

---
+++ Tugas 9/index.js
@@ -0,0 +1,64 @@
+//menghitung luas persegi panjang//
+let panjang = 5
+let lebar = 3
+let hasil = panjang * lebar
+console.log(hasil)
+
+//menghitung rata-rata 2 angka//
+let angka1 = 8
+let angka2 = 4
+let rataRata = (angka1+angka2)/2
+console.log(rataRata)
+
+//menghitung luas segitiga//
+let alas = 6
+let tinggi = 4
+let hasil1 =  alas * tinggi/2
+
+console.log(hasil1)
+
+
+//menghitung sisa hasil bagi//
+let x=10
+let y=5
+let sisahasilbagi= x % y
+console.log(sisahasilbagi)
+
+//mengonversi waktu//
+let detik = 80
+
+let hasil3 = detik / 60
+console.log(hasil3)
+
+//menghitung keliling persegi dengan perkalian dan penjumlahan//
+let sisi = 8
+let keliling = 4 * sisi
+console.log(keliling)
+
+let sisi1 = 10
+let keliling1 = sisi + sisi + sisi + sisi
+console.log(keliling1)
+
+//menghitung total waktu//
+let waktuTugas1 = 40
+let waktuTugas2 = 20
+let totalWaktu = waktuTugas1 + waktuTugas2
+console.log(totalWaktu)
+
+//menghitung volume silinder//
+let phi = 3.14;
+let jari = 9;
+let tinggiVolume = 24;
+let volume = phi * (jari * 2) * tinggiVolume;
+console.log(volume)
+
+//menghitung rata-rata 3 angka dengan penjumlahan dan pembagian//
+let angka3 = 12
+let angka4 = 16
+let angka5 = 18
+let rata_rata1 = (angka3 + angka4 + angka5)/3
+console.log(rata_rata1)
+
+//menghitung rata-rata 3 angka dengan penjumlahan dan pembagian//
+let rata_rata2 = (angka3 + angka4 + angka5)/3
+console.log(rata_rata2)
---
+++ tes/index.js
@@ -0,0 +1,7 @@
+let isMahasiswa = false
+
+if (isMahasiswa == true) {
+    console.log("akumahasiswa")
+}else if(isMahasiswa == false){
+    console.log("akubukanmahasiswa")
+}
